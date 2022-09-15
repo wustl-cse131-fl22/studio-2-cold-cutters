7222202d7547b6e1ed92858c122a5fd777db63a0
gracehuang <gracehuang@localhost>
2022-09-15 11:48:19 -0500

    made more variables

--- src/studio2/ruin.java
+++ src/studio2/ruin.java
@@ -1,13 +1,25 @@
 package studio2;
+import java.util.Scanner;
 
 public class ruin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		double startAmount;
+		Scanner in = new Scanner(System.in);
+	
 		double winChance;
 		double winLimit;
+		int day;
+		boolean game = true;
+		
+		
+		System.out.println("Enter Starting Amount: ");
+		int startAmount = in.nextInt();
+		int amount = startAmount;
+		
+		while(game) {
+			
+		}
 
 	}
 
