7c10c16d98ae74e615a1b4bda8ce15be10cfa964
Nick <111998485+NicholasEng26@users.noreply.github.com>
2022-09-15 12:01:44 -0500

    Done

--- src/studio2/ruin.java
+++ src/studio2/ruin.java
@@ -8,9 +8,12 @@ public class ruin {
 		Scanner in = new Scanner(System.in);
 	
 		double winChance;
-		double winLimit = 0;
-		int day;
+		double winLimit;
+		double ruinLimit;
+		int day = 1;
 		boolean game = true;
+		int totalSimulations = 0;
+		double random = Math.random();
 		
 		
 		System.out.println("Enter Starting Amount: ");
@@ -18,9 +21,20 @@ public class ruin {
 		int amount = startAmount;
 		
 		while(game) {
-			System.out.println ("What is your win limit?");
-			winLimit = winLimit/100; 
+			System.out.println("Day: " + day);
 			
+			System.out.println ("What is your win chance?");
+			winChance = in.nextDouble()/100.0; 
+			System.out.println ("What is your win limit?");
+			winLimit = in.nextDouble()/100.0; 
+			System.out.println ("What is your ruin limit?");
+			ruinLimit = in.nextDouble()/100.0; 
+			
+			/* Check for Ruin or Success */
+			
+			// Next Day
+			
+			day++;
 					
 		}
 
