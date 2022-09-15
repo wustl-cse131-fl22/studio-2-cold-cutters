8622749a63dd582cb3450efe0f9fedac8ade48da
Nick <111998485+NicholasEng26@users.noreply.github.com>
2022-09-15 11:40:38 -0500

    added vars

--- src/studio2/ruin.java
+++ src/studio2/ruin.java
@@ -4,6 +4,11 @@ public class ruin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		double startAmount;
+		double winChance;
+		double winLimit;
+		
 
 	}
 
